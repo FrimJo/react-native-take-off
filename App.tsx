9e32f416462a0b9be1c7f7eceabc25eb56e06d43
Fredrik Johansson <fredrik.johansson@thnx.se>
2020-08-15 14:50:03 +0200

    Add quality of life imporvements with eslint, prettier, husky and patch-package

--- App.tsx
+++ App.tsx
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { css } from '@emotion/native'
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View
+      style={css`
+        flex: 1;
+        background-color: #fff;
+        align-items: center;
+        justify-content: center;
+      `}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
